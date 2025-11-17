f6f64e2664d010e89c243b3f3ddfe06d3dd45c0a
Heyuan Zeng <hi@universe.observer>
2025-11-17 21:55:11 +0000

    More flexible hex string printer

--- Sources/BinaryParseKit/Printer/HexStringPrinter.swift
+++ Sources/BinaryParseKit/Printer/HexStringPrinter.swift
@@ -5,23 +5,54 @@
 //  Created by Larry Zeng on 11/17/25.
 //
 
-public struct HexStringPrinter: ParsablePrinter {
+public protocol HexStringPrinterFormatter {
+    typealias ByteSource = [UInt8]
+
+    func format(bytes: ByteSource) -> String
+}
+
+public struct DefaultHexStringPrinterFormatter: HexStringPrinterFormatter {
     let separator: String
     let prefix: String
 
-    public func print(_ intel: PrinterIntel) throws(ParsablePrinterError) -> String {
-        try ByteArrayPrinter()
-            .print(intel)
+    public func format(bytes: ByteSource) -> String {
+        bytes
             .map { unsafe String(format: "\(prefix)%02X", $0) }
             .joined(separator: separator)
     }
 }
 
-public extension ParsablePrinter where Self == HexStringPrinter {
+public struct HexStringPrinter<F: HexStringPrinterFormatter>: ParsablePrinter {
+    public let formatter: F
+
+    public init(separator: String = "", prefix: String = "") where F == DefaultHexStringPrinterFormatter {
+        formatter = DefaultHexStringPrinterFormatter(
+            separator: separator,
+            prefix: prefix,
+        )
+    }
+
+    public init(formatter: F) {
+        self.formatter = formatter
+    }
+
+    public func print(_ intel: PrinterIntel) throws(ParsablePrinterError) -> String {
+        let byteSource = try ByteArrayPrinter().print(intel)
+        return formatter.format(bytes: byteSource)
+    }
+}
+
+public extension ParsablePrinter where Self == HexStringPrinter<DefaultHexStringPrinterFormatter> {
     static func hexString(separator: String = "", prefix: String = "") -> Self {
         HexStringPrinter(
             separator: separator,
             prefix: prefix,
         )
+    }
+}
+
+public extension ParsablePrinter {
+    static func hexString<T: HexStringPrinterFormatter>(formatter: T) -> HexStringPrinter<T> {
+        HexStringPrinter(formatter: formatter)
     }
 }
